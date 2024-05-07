d38f566849b537ba3874d8fa0edc114a7cb86b63
Jaygrison <Justing2014@yahoo.com>
2024-05-07 12:51:09 -0500

    updatedFinal

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+
+
 let stars = [];
 let planets = [];
 let cam;
+let model;
+let img;
+
+
+
+function preload() {
+  img = []
+  img[0] = loadImage("texture/ear0xuu2.jpg");
+  img[1] = loadImage("texture/as10-31-4537.jpg");
+  img[2] = loadImage("texture/ven0aaa2.jpg");
+  model = loadModel("texture/Dawn_19.stl", true, handleModel, handleError);
+}
 
 function setup() {
-  createCanvas(800, 600, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   cam = createCamera();
-  cam.setPosition(0, 0, 500); // Set initial camera position
+  cam.setPosition(0, 0, 700); // Set initial camera 
+  angleMode(DEGREES)
   
   // Create stars
   for (let i = 0; i < 800; i++) {
@@ -14,17 +29,36 @@ function setup() {
   
   // Create planets
   for (let i = 0; i < 5; i++) {
-    planets.push(new Planet(40, random(TWO_PI)));
+    planets.push(new Planet(23, random(TWO_PI)));
   }
 }
+  
 
 function draw() {
   background(0);
 
 
+  function biggerPlanet(x, y, radius) {
+    rotateY(frameCount * .005);
+    texture(img[2]);
+    translate(x - width / 2, y - height / 2);
+    sphere(radius);
+    
+  }  
+
+  orbitControl();
   
+  //translate(0, 0, -200);
+  //model(model);
 
-
+  let angle = frameCount * 0.02;
+  rotate(angle);
+  texture(img[0]);
+  noStroke();
+  sphere(75);
+  
+  
+  
 
   // Control camera movement
   if (keyIsDown(UP_ARROW)) {
@@ -59,21 +93,22 @@ function draw() {
     planet.display();
     planet.update();
   }
+  biggerPlanet(10, 0, 250);
 }
 
 class Planet {
   constructor(radius, angle) {
     this.radius = radius;
     this.angle = angle;
-    this.speed = random(0.0001, 0.0003);
+    this.speed = random(0.9, 1.8);
   }
   
   display() {
     push();
     rotateY(this.angle);
     translate(300, 0, 0);
-    normalMaterial();
-    fill(255, 0, 0);
+    noStroke();
+    texture(img[1]);
     sphere(this.radius);
     pop();
   }
