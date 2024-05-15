2e077c4c664ec1d9bd8df76cbade978d9fbb50b2
Jaygrison <Justing2014@yahoo.com>
2024-05-14 23:14:56 -0500

    final changes

--- script.js
+++ script.js
@@ -1,33 +1,83 @@
 
+// We create a planet class. The contructor will have the radius param and angle param
+// we update the properties by the parameters
+// The speed variable will use the random function
+class Planet {
+  constructor(radius, angle) {
+    this.radius = radius;
+    this.angle = angle;
+    this.speed = random(0.9, 1.8);
+  }
+  
+// we create a method called display to show the planet
+// we use the push and pop function to save what is already coded before and after
+// we rotate the planet by the angle variable
+// we translate the planet to the x y z coordinates stated
+// add a nostroke, with a texture given by a img using an element in an array
+// we create the planet by the radius 
+// we go back to the regular settings, pop
+  display() {
+    push();
+    rotateY(this.angle);
+    translate(300, 0, 0);
+    noStroke();
+    texture(img[1]);
+    sphere(this.radius);
+    pop();
+  }
+// we continue to update the angle and speed which is random
+  update() {
+    this.angle += this.speed;
+  }
+}
+
+// we create arrays and variables we will use for the creation
+// an array for multiple planets
+// an arry for multiple stars
 
 let stars = [];
 let planets = [];
 let cam;
-let model;
+let mymodel;
 let img;
 
-
+// we preload images for the textures
+// since we have 3 images, we save them in an array called img
+// we upload a model and set to true. 
 
 function preload() {
   img = []
   img[0] = loadImage("texture/ear0xuu2.jpg");
   img[1] = loadImage("texture/as10-31-4537.jpg");
   img[2] = loadImage("texture/ven0aaa2.jpg");
-  model = loadModel("texture/Dawn_19.stl", true, handleModel, handleError);
+  mymodel = loadModel("Dawn_19.stl", true);
+
+  
 }
 
+// with in setup function we create the canvas to the window size
+// enter into WEBGL mode for the 3D environment
+// create a cam using createcamera function
+// set the camera position to the specified coordinates
+// set angle mode to degree for the rotation around center
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   cam = createCamera();
   cam.setPosition(0, 0, 700); // Set initial camera 
   angleMode(DEGREES)
   
-  // Create stars
+// we Create the stars
+// use a for loop for 800 iterations
+// we push new stars within the array
+// we create vectors for the shape with random width and height
+// we hard code the z coordinate within random
   for (let i = 0; i < 800; i++) {
     stars.push(createVector(random(-width, width), random(-height, height), random(-1000, 1000)));
   }
-  
-  // Create planets
+// We Create the planets
+// we use another loop for 5 planet iteration
+// within loop we push the new planet object with a radius of 23
+// with the angle being random and we set to two pi for the radians in 360 degrees
   for (let i = 0; i < 5; i++) {
     planets.push(new Planet(23, random(TWO_PI)));
   }
@@ -37,7 +87,11 @@ function setup() {
 function draw() {
   background(0);
 
-
+// we create a function for another different size planet
+// we rotate by framecount times 5 thousandths
+// give one of the textures within the img array
+// move the planet to what x or y is minus the width and height divided by 2
+// create the planey by the given radius
   function biggerPlanet(x, y, radius) {
     rotateY(frameCount * .005);
     texture(img[2]);
@@ -46,11 +100,29 @@ function draw() {
     
   }  
 
-  orbitControl();
-  
-  //translate(0, 0, -200);
-  //model(model);
 
+// we use orbit control to be able to use the mouse to move the canvas 
+  orbitControl();
+
+// add a push to save the code
+// add directional light and ambient light and material to set up a color for a model
+// we use translate to put model into a certain coordinate
+// we call the model using model function
+  push();
+  directionalLight(255, 255, 255, 0.5, 0.5, -1);
+  ambientLight(250);
+  ambientMaterial(255, 0, 0);
+  translate(0, 100, 400);
+  model(mymodel);
+  
+// we pop out of the certain code back to regular
+  
+  pop();
+
+// we create another plane
+// we create a angle for the planet
+// texture will be an element from the array
+// we create the planet using sphere function
   let angle = frameCount * 0.02;
   rotate(angle);
   texture(img[0]);
@@ -60,7 +132,10 @@ function draw() {
   
   
 
-  // Control camera movement
+// we create keydown presses for camera movement
+// we use key is down function for up, down, left and right
+// we move y coordinate up 5 or down 5 pixels when key is up and down
+// we move x coordinate up 5 or down 5 pixels when key is left or right
   if (keyIsDown(UP_ARROW)) {
     cam.move(0, -5, 0);
   }
@@ -74,7 +149,13 @@ function draw() {
     cam.move(5, 0, 0);
   }
   
-  // Display stars
+// we push again for the drawing of the stars
+// add no stroke
+// we use for loop for the array stars
+// we create variables for x, y, and z which is random in setup
+// we include those variables within translate function
+// create the stars by sphere
+// then another translate function for negative coordinates
   push();
   noStroke();
   fill(255);
@@ -82,38 +163,23 @@ function draw() {
     let sx = star.x;
     let sy = star.y;
     let sz = star.z;
+
     translate(sx, sy, sz);
     sphere(3);
     translate(-sx, -sy, -sz);
   }
+
+// we pop back to regular
   pop();
+
   
-  // Display and update planets
+// we then Display and update planets using for loop 
+// we access the array and within the loop we use the display method and update method from the planet class
   for (let planet of planets) {
     planet.display();
     planet.update();
   }
-  biggerPlanet(10, 0, 250);
+// we lastly call the biggerplanet function to create the final planet
+  biggerPlanet(0, 0, 200);
 }
 
-class Planet {
-  constructor(radius, angle) {
-    this.radius = radius;
-    this.angle = angle;
-    this.speed = random(0.9, 1.8);
-  }
-  
-  display() {
-    push();
-    rotateY(this.angle);
-    translate(300, 0, 0);
-    noStroke();
-    texture(img[1]);
-    sphere(this.radius);
-    pop();
-  }
-  
-  update() {
-    this.angle += this.speed;
-  }
-}
